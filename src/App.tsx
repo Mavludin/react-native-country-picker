535bc41a6bef49f3124d2325d935cc0316225f24
Mavludin <imevlud@gmail.com>
2023-06-05 15:09:30 +0300

    display test countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import {Text, View} from 'react-native';
+import React from 'react';
+import countries from './utils/countries.json';
+import {AfIcon} from './Af';
 
 function App() {
   return (
     <View>
       <Text>123</Text>
+      {countries.slice(0, 5).map(country => {
+        return (
+          <View>
+            <AfIcon />
+            <Text>{country.name}</Text>
+          </View>
+        );
+      })}
     </View>
   );
 }
