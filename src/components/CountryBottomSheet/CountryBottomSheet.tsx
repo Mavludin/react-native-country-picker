96f14d513c532657cd653a06523d03dc6281a679
Mavludin <imevlud@gmail.com>
2023-06-09 15:49:14 +0300

    handle double language import

--- src/components/CountryBottomSheet/CountryBottomSheet.tsx
+++ src/components/CountryBottomSheet/CountryBottomSheet.tsx
@@ -5,6 +5,7 @@ import BottomSheet from '@gorhom/bottom-sheet';
 import {View, useWindowDimensions, Text} from 'react-native';
 import {
   CountryItem,
+  LanguageCode,
   countriesByLanguage,
   deviceLanguage,
 } from '../../utils/countries';
@@ -13,29 +14,60 @@ import {getCountry} from 'react-native-localize';
 
 const SNAP_POINTS = ['10%', '100%'];
 
+export type DoubleCountryItem =
+  | Record<Partial<LanguageCode>, CountryItem[]>
+  | undefined;
+export type SingleCountryItem = CountryItem[] | undefined;
+
 export const CountryBottomSheet = () => {
   const sheetRef = useRef<BottomSheet>(null);
 
   const {height: screenHeight} = useWindowDimensions();
 
-  const [countries, setCountries] = useState<CountryItem[] | undefined>();
-  const [defaultCountry, setDefaultCountry] = useState<CountryItem>();
+  const [countries, setCountries] = useState<
+    DoubleCountryItem | SingleCountryItem
+  >();
+  const [defaultCountry, setDefaultCountry] = useState<
+    CountryItem | undefined
+  >();
 
   const [error, setError] = useState<string | null>(null);
 
-  const importData = () =>
-    countriesByLanguage[deviceLanguage]()
-      .then(json => json.default as CountryItem[])
-      .then(data => {
+  const importData = async () => {
+    if (deviceLanguage !== 'en') {
+      try {
+        const lang1 = await countriesByLanguage[deviceLanguage]();
+        const lang2 = await countriesByLanguage.en();
+
+        const data = {
+          [deviceLanguage]: lang1.default,
+          en: lang2.default,
+        } as DoubleCountryItem;
+
+        setDefaultCountry(
+          data?.[deviceLanguage]?.find(
+            country => country.alpha2 === getCountry(),
+          ),
+        );
+        setCountries(data);
+      } catch (error) {
+        console.error('Error importing data:', error);
+        setError('Error importing data');
+      }
+    } else {
+      try {
+        const json = await countriesByLanguage[deviceLanguage]();
+        const data = json.default as CountryItem[];
         setDefaultCountry(
           data?.find(country => country.alpha2 === getCountry()),
         );
         setCountries(data.filter(country => country.alpha2 !== getCountry()));
-      })
-      .catch(error => {
+      } catch (error) {
         console.error('Error importing data:', error);
         setError('Error importing data');
-      });
+      }
+    }
+  };
 
   if (!deviceLanguage) {
     return null;
--- src/components/CountryList/CountryList.tsx
+++ src/components/CountryList/CountryList.tsx
@@ -1,12 +1,16 @@
 import {BottomSheetFlatList, BottomSheetTextInput} from '@gorhom/bottom-sheet';
 import React, {useState} from 'react';
-import {CountryItem} from '../../utils/countries';
+import {CountryItem, deviceLanguage} from '../../utils/countries';
 import {HeaderCountry} from '../HeaderCountry/HeaderCountry';
 import {Country} from '../Country/Country';
 import {StyleSheet} from 'react-native';
+import {
+  DoubleCountryItem,
+  SingleCountryItem,
+} from '../CountryBottomSheet/CountryBottomSheet';
 
 type Props = {
-  countries: CountryItem[];
+  countries: DoubleCountryItem | SingleCountryItem;
   defaultCountry?: CountryItem;
 };
 
@@ -23,22 +27,45 @@ export const CountryList = ({countries, defaultCountry}: Props) => {
     <>
       <BottomSheetTextInput style={styles.input} placeholder="Search" />
 
-      <BottomSheetFlatList
-        data={countries}
-        ListHeaderComponent={<HeaderCountry defaultCountry={defaultCountry} />}
-        renderItem={({item: country}) => {
-          const isActive = country.alpha2 === selectedCountry?.alpha2;
-          return (
-            <Country
-              isActive={isActive}
-              onSelect={onSelect}
-              country={country}
-            />
-          );
-        }}
-        keyExtractor={country => country?.alpha2 || ''}
-        contentContainerStyle={styles.contentContainer}
-      />
+      {Array.isArray(countries) ? (
+        <BottomSheetFlatList
+          data={countries}
+          ListHeaderComponent={
+            <HeaderCountry defaultCountry={defaultCountry} />
+          }
+          renderItem={({item: country}) => {
+            const isActive = country.alpha2 === selectedCountry?.alpha2;
+            return (
+              <Country
+                isActive={isActive}
+                onSelect={onSelect}
+                country={country}
+              />
+            );
+          }}
+          keyExtractor={country => country?.alpha2 || ''}
+          contentContainerStyle={styles.contentContainer}
+        />
+      ) : (
+        <BottomSheetFlatList
+          data={countries?.[deviceLanguage]}
+          ListHeaderComponent={
+            <HeaderCountry defaultCountry={defaultCountry} />
+          }
+          renderItem={({item: country}) => {
+            const isActive = country.alpha2 === selectedCountry?.alpha2;
+            return (
+              <Country
+                isActive={isActive}
+                onSelect={onSelect}
+                country={country}
+              />
+            );
+          }}
+          keyExtractor={country => country?.alpha2 || ''}
+          contentContainerStyle={styles.contentContainer}
+        />
+      )}
     </>
   );
 };
